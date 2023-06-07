b9a53962527e47b4ead24a0a5be56d9c7a15f5a1
alessandro marangoni <alessandromarangoni980@gmail.com>
2023-06-07 18:02:06 +0200

    ristampo in un altra pagina con lunghezza

--- form-compiler.php
+++ form-compiler.php
@@ -4,10 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>form compiler</title>
 </head>
 <body>
     <?php
     ?>
+    <div>
+    <form action="form-render.php" method="get">
+        <input type="text" name="paragrafo" id="paragrafo" placeholder="inserisci il tuo testo">
+        <button type="submit"> verifica </button>
+    </form>
+    </div>
 </body>
 </html>
--- form-render.php
+++ form-render.php
@@ -4,10 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>form render</title>
 </head>
 <body>
     <?php
+    $paragrafoRender = $_GET["paragrafo"];
+    $paragrafoArray = explode(" ", $paragrafoRender);
     ?>
+
+    <div>
+        <!-- qui il paragrafo stampato -->
+        <h2>
+            <?php echo $paragrafoRender?>
+        </h2>
+        <p>
+        <?php   var_dump($paragrafoRender)?>
+        </p>
+        <!-- qui il paragrafo stampato  con la sua lunghezza-->
+        <p>
+            <?php  echo $paragrafoRender . " <br> " .
+            'il paragrafo è lungo: ' . strlen($paragrafoRender) . ' ' . 'caratteri' ?>
+        </p>
+        <p>
+        <?php   var_dump($paragrafoArray)?>
+        </p>
+        
+    </div>
 </body>
 </html>
